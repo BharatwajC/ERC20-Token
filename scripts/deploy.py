d775900ea598b21b9f708f35a1d9cc7b7e062152
Bharatwaj C <77624728+BharatwajC@users.noreply.github.com>
2022-02-24 18:05:42 +0530

    deploy function

---
+++ scripts/deploy.py
@@ -0,0 +1,14 @@
+from scripts.helpful_scripts import get_account
+from brownie import OurToken,accounts
+from web3 import Web3
+
+initial_supply = Web3.toWei(1000, "ether")
+
+def deploy_contract():
+    account = get_account()
+    
+    our_token = OurToken.deploy(initial_supply,{"from":account})
+    print(our_token.name())
+
+def main():
+    deploy_contract()
